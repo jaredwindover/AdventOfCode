369df781bd43fbe925825748c42f6bdec3154fb2
Jared Windover <jaredwindover@gmail.com>
2021-12-13 23:07:04 -0500

    Day 12 part 2

--- 2021/Haskell/d12/src/Lib.hs
+++ 2021/Haskell/d12/src/Lib.hs
@@ -17,7 +17,7 @@ first :: (a -> Bool) -> [a] -> a
 first predicate = head.(dropWhile $ not.predicate)
 
 start :: [SoFar]
-start = [("start", ["start"], singleton "start")]
+start = [("start", ["start"], singleton "start", False)]
 
 printSet :: Set String -> String
 printSet s = show $ elems s
@@ -34,26 +34,36 @@ makeConnections xs = foldl upsert Data.Map.empty (xs ++ map (\(a, b) -> (b, a)) 
 splitOnDashes :: [Char] -> [String]
 splitOnDashes = splitWhen (\x -> x == '-')
 
-type SoFar = (String, [String], Set String)
+type SoFar = (String, [String], Set String, Bool)
 
 type Accumulator = ([SoFar], [[String]])
 
 isSmall :: String -> Bool
 isSmall s = isLower $ s !! 0
 
+getUpdate :: SoFar -> String -> SoFar
+getUpdate (c, path, visited, doubled) potential = (potential, path ++ [potential], newVisited, newDoubled)
+  where
+    newVisited
+      | isSmall c = insert c visited
+      | otherwise = visited
+    newDoubled = doubled || (member potential visited)
+
+getNewOptions :: SoFar -> [String]-> [SoFar]
+getNewOptions (c, path, visited, doubled) potentials = map (\p -> getUpdate (c, path, visited, doubled) p) realPotentials
+  where
+    realPotentials = filter (\s -> (s/="end") && (s/="start") && ((not doubled) || (not $ isSmall s) || (not $ member s visited))) potentials
+
+
 update :: Connections -> Accumulator -> Accumulator
 update _ ([], paths) = ([], paths)
-update cs ((c, path, visited):rest, paths) = (newOptions ++ rest, newPaths)
+update cs ((c, path, visited, doubled):rest, paths) = (newOptions ++ rest, newPaths)
     where
-      newOptions = map (\c' -> (c', path ++ [c'], newVisited)) realPotentials :: [SoFar]
-      newVisited
-        | isSmall c = insert c visited
-        | otherwise = visited
+      newOptions = getNewOptions (c, path, visited, doubled) potentials
       newPaths
         | elem "end" potentials = (path ++ ["end"]):paths
         | otherwise = paths
       potentials = elems $ cs ! c
-      realPotentials = filter (\s -> (s/="end") && ((not $ isSmall s) || (not $ member s visited))) potentials
 
 findPaths :: Connections -> IO [[String]]
 findPaths connections = do
